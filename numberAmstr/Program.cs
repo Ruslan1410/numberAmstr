6c0dbb35395a69ad232313dd2b1b0c467e7504e3
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-15 20:27:29 +0300

    number Amstronh with chatGPT

--- numberAmstr/Program.cs
+++ numberAmstr/Program.cs
@@ -6,10 +6,62 @@ using System.Threading.Tasks;
 
 namespace numberAmstr
 {
-    internal class Program
+    using System;
+
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Введите число: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number) && number >= 0)
+            {
+                if (IsArmstrongNumber(number))
+                {
+                    Console.WriteLine($"{number} - это число Армстронга.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} - не является числом Армстронга.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода. Пожалуйста, введите неотрицательное целое число.");
+            }
+        }
+
+        // Метод для проверки, является ли число числом Армстронга
+        static bool IsArmstrongNumber(int num)
+        {
+            int originalNumber = num;
+            int numberOfDigits = CountDigits(num);
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum += (int)Math.Pow(digit, numberOfDigits);
+                num /= 10;
+            }
+
+            return sum == originalNumber;
+        }
+
+        // Метод для подсчета количества цифр в числе
+        static int CountDigits(int num)
+        {
+            int count = 0;
+
+            while (num > 0)
+            {
+                num /= 10;
+                count++;
+            }
+
+            return count;
         }
     }
+
 }
